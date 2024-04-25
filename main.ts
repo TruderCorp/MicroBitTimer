fbd16684b6118261fb5b741b344cca3435e3fb66
TruderCorp <167313975+TruderCorp@users.noreply.github.com>
2024-04-25 09:42:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let minutes = 0
+let TurnOff = false
+input.onButtonPressed(Button.A, function () {
+    minutes += 5
+    basic.showNumber(minutes)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    while (minutes > 0) {
+        // 1 minute = 60,000 milliseconds
+        basic.pause(60000)
+        minutes += -1
+    }
+    while (1 == 1) {
+        music.setVolume(255)
+        music.playTone(1000, music.beat(BeatFraction.Whole))
+        music.playTone(1500, music.beat(BeatFraction.Whole))
+        music.playTone(2000, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    minutes += -5
+    basic.showNumber(minutes)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    TurnOff = true
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(minutes)
+})
